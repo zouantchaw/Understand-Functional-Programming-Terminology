b84a079d4c169ac5b7201ca7d7e22ee498665e8a
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-11-05 18:07:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+
+//Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. 
+//Note that the getTea function has been modified so it now takes a function as the first argument.
+
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27)
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13)
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
